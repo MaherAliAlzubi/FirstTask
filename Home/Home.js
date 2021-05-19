16ea5c03512203b4b7f192a25e4e6e26d1f026b2
Maher Alzubi <maher.alzubi@optimumpartners.co>
2021-05-19 05:48:43 -0400

    uploading the project

---
+++ Home/Home.js
@@ -0,0 +1,156 @@
+//DOM
+
+var obj = document.getElementsByClassName("HomeBody");
+//Variables
+let isDarkMode = true;
+// Functions
+function changeDarkMode() {
+  isDarkMode = !isDarkMode;
+  if (isDarkMode) {
+    document.getElementById("HomeBody").style.backgroundColor = "#1b2631";
+    document.getElementById("HomeHeader").style.backgroundColor = "#2e4053";
+    document.getElementById("HomeHeaderTitle").style.color = "white";
+    document.getElementById("HomeHeaderDarkMode").style.color = "white";
+    let card = document.getElementsByClassName("HomeCountryCard");
+    let countryName = document.getElementsByClassName(
+      "HomeCountryCardCountryName"
+    );
+    let countryPopulation = document.getElementsByClassName(
+      "HomeCountryCardCountryPopulation"
+    );
+    let countryReigon = document.getElementsByClassName(
+      "HomeCountryCardCountryReigon"
+    );
+    let countryCapital = document.getElementsByClassName(
+      "HomeCountryCardCountryCapital"
+    );
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.backgroundColor = "#2e4053";
+      countryName[i].style.color = "white";
+      countryPopulation[i].style.color = "white";
+      countryReigon[i].style.color = "white";
+      countryCapital[i].style.color = "white";
+    }
+  } else {
+    document.getElementById("HomeBody").style.backgroundColor = "#ECEAEA";
+    document.getElementById("HomeHeader").style.backgroundColor = "white";
+    document.getElementById("HomeHeaderTitle").style.color = "#000000";
+    document.getElementById("HomeHeaderDarkMode").style.color = "#000000";
+    let card = document.getElementsByClassName("HomeCountryCard");
+    let countryName = document.getElementsByClassName(
+      "HomeCountryCardCountryName"
+    );
+    let countryPopulation = document.getElementsByClassName(
+      "HomeCountryCardCountryPopulation"
+    );
+    let countryReigon = document.getElementsByClassName(
+      "HomeCountryCardCountryReigon"
+    );
+    let countryCapital = document.getElementsByClassName(
+      "HomeCountryCardCountryCapital"
+    );
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.backgroundColor = "white";
+      countryName[i].style.color = "#000000";
+      countryPopulation[i].style.color = "#000000";
+      countryReigon[i].style.color = "#000000";
+      countryCapital[i].style.color = "#000000";
+    }
+  }
+}
+
+function displayData() {
+  fetch("https://restcountries.eu/rest/v2/all", {
+    method: "GET",
+  }).then((res) =>
+    res.json().then((result) => {
+      for (let i = 0; i < result.length; i++) {
+        createCard(
+          result[i].flag,
+          result[i].name,
+          result[i].population,
+          result[i].region,
+          result[i].capital,
+          result[i].alpha2Code
+        );
+      }
+    })
+  );
+}
+displayData();
+
+function createCard(
+  countryFlagParam,
+  countryNameParam,
+  countryPopulationValueParam,
+  countryReigonValueParam,
+  countryCapitalValueParam,
+  code
+) {
+  let grid = document.getElementsByClassName("cardsGrid")[0];
+  let card = document.createElement("div");
+
+  let countryFlag = document.createElement("img");
+
+  let countryName = document.createElement("h3");
+
+  let populationDiv = document.createElement("div");
+  let countryPopulation = document.createElement("p");
+  let countryPopulationValue = document.createElement("p");
+
+  let reigonDiv = document.createElement("div");
+  let countryReigon = document.createElement("p");
+  let countryReigonValue = document.createElement("p");
+
+  let capitalDiv = document.createElement("div");
+  let countryCapital = document.createElement("p");
+  let countryCapitalValue = document.createElement("p");
+
+  card.className = "HomeCountryCard";
+
+  countryFlag.src = countryFlagParam;
+  countryFlag.className = "HomeCountryCardImage";
+
+  countryName.innerHTML = countryNameParam;
+  countryName.className = "HomeCountryCardCountryName";
+
+  populationDiv.className = "HomeCountryCardCountryPopulation";
+  countryPopulation.innerHTML = "Population : ";
+  countryPopulationValue.innerHTML = countryPopulationValueParam;
+
+  reigonDiv.className = "HomeCountryCardCountryReigon";
+  countryReigon.innerHTML = "Reigon : ";
+  countryReigonValue.innerHTML = countryReigonValueParam;
+
+  capitalDiv.className = "HomeCountryCardCountryCapital";
+  countryCapital.innerHTML = "Capital : ";
+  countryCapitalValue.innerHTML = countryCapitalValueParam;
+
+  countryPopulation.className = "HomeBoldKey";
+  countryReigon.className = "HomeBoldKey";
+  countryCapital.className = "HomeBoldKey";
+  capitalDiv.appendChild(countryCapital);
+  capitalDiv.appendChild(countryCapitalValue);
+
+  reigonDiv.appendChild(countryReigon);
+  reigonDiv.appendChild(countryReigonValue);
+
+  populationDiv.appendChild(countryPopulation);
+  populationDiv.appendChild(countryPopulationValue);
+
+  card.appendChild(countryFlag);
+  card.appendChild(countryName);
+  card.appendChild(populationDiv);
+  card.appendChild(reigonDiv);
+  card.appendChild(capitalDiv);
+
+  grid.appendChild(card);
+  card.addEventListener("click", function () {
+    window.open(
+      "/home/maher/Desktop/firstTask/View/view.html?code=" +
+        code +
+        "&dark=" +
+        isDarkMode
+    );
+  });
+}
---
+++ View/view.js
@@ -0,0 +1,172 @@
+// https://restcountries.eu/rest/v2/callingcode/963
+
+let code = document.location.search.substring(6, 8);
+let isDarkMode = document.location.search.substring(14, 15);
+if (isDarkMode == "t") isDarkMode = true;
+else isDarkMode = false;
+displayTheme(isDarkMode);
+function getDetails() {
+  fetch("https://restcountries.eu/rest/v2/alpha/" + code, {
+    method: "GET",
+  }).then((res) =>
+    res.json().then((result) => {
+      let currencies = "";
+      let languages = "";
+      for (let i = 0; i < result.languages.length; i++)
+        languages += result.languages[i].name + ",";
+      for (let i = 0; i < result.currencies.length; i++)
+        currencies += result.currencies[i].code + ",";
+      currencies = currencies.substring(0, currencies.length - 1);
+      languages = languages.substring(0, languages.length - 1);
+      manipulate(
+        result.flag,
+        result.name,
+        result.nativeName,
+        result.population,
+        result.region,
+        result.subregion,
+        result.capital,
+        result.topLevelDomain[0],
+        currencies,
+        languages,
+        result.borders
+      );
+      console.log(result);
+    })
+  );
+}
+
+getDetails();
+function manipulate(
+  countryFlag,
+  countryName,
+  nativeNameParam,
+  populationParam,
+  regionParam,
+  subRegionParam,
+  capitalParam,
+  topLevelDomainParam,
+  currenciesParam,
+  languagesParam,
+  bordersParam
+) {
+  let image = document.getElementsByClassName("ViewFlag")[0];
+  image.src = countryFlag;
+  let name = document.getElementsByClassName("ViewCountryName")[0];
+  name.innerHTML = countryName;
+
+  let nativeName = document.getElementsByClassName("ViewCountryNativeName")[0];
+  nativeName.innerHTML = nativeNameParam;
+  let population = document.getElementsByClassName("ViewCountryPopulation")[0];
+  population.innerHTML = populationParam;
+  let region = document.getElementsByClassName("ViewCountryReigon")[0];
+  region.innerHTML = regionParam;
+  let subRegion = document.getElementsByClassName("ViewCountrySubReigon")[0];
+  subRegion.innerHTML = subRegionParam;
+
+  let capital = document.getElementsByClassName("ViewCountryCapital")[0];
+  capital.innerHTML = capitalParam;
+
+  let topLevelDomain = document.getElementsByClassName(
+    "ViewCountryTopLevelDomain"
+  )[0];
+  topLevelDomain.innerHTML = topLevelDomainParam;
+
+  let currencies = document.getElementsByClassName("ViewCountryCurrencies")[0];
+  currencies.innerHTML = currenciesParam;
+
+  let languages = document.getElementsByClassName("ViewCountryLanguages")[0];
+  languages.innerHTML = languagesParam;
+  for (let i = 0; i < bordersParam.length; i++) {
+    createNameCard(bordersParam[i]);
+  }
+}
+function createNameCard(name) {
+  let divNode = document.createElement("div");
+  let pNode = document.createElement("p");
+  let cards = document.getElementsByClassName("nameCards")[0];
+  divNode.className = "nameCard";
+  pNode.innerHTML = name;
+  pNode.className = "nameCardPara";
+  divNode.appendChild(pNode);
+  cards.appendChild(divNode);
+}
+function displayTheme(isDarkMode) {
+  if (isDarkMode) {
+    document.getElementById("HomeBody").style.backgroundColor = "#1b2631";
+    let header = document.getElementById("HomeHeader");
+    document.getElementById("HomeHeader").style.backgroundColor = "#2e4053";
+    document.getElementById("HomeHeaderTitle").style.color = "white";
+    document.getElementById("HomeHeaderDarkMode").style.color = "white";
+    header.style.backgroundColor = "#2e4053";
+    document.getElementsByClassName("ViewCountryName")[0].style.color = "white";
+    let keys = document.getElementsByClassName("ViewKey");
+    for (let i = 0; i < keys.length; i++) {
+      keys[i].style.color = "white";
+    }
+    document.getElementsByClassName("ViewCountryNativeName")[0].style.color =
+      "white";
+    document.getElementsByClassName("ViewCountryPopulation")[0].style.color =
+      "white";
+    document.getElementsByClassName("ViewCountryReigon")[0].style.color =
+      "white";
+    document.getElementsByClassName("ViewCountrySubReigon")[0].style.color =
+      "white";
+    document.getElementsByClassName("ViewCountryCapital")[0].style.color =
+      "white";
+    document.getElementsByClassName(
+      "ViewCountryTopLevelDomain"
+    )[0].style.color = "white";
+    document.getElementsByClassName("ViewCountryCurrencies")[0].style.color =
+      "white";
+    document.getElementsByClassName("ViewCountryLanguages")[0].style.color =
+      "white";
+    let nameCards = document.getElementsByClassName("nameCard");
+    let bordersName = document.getElementsByClassName("nameCardPara");
+    for (let i = 0; i < nameCards.length; i++) {
+      nameCards[i].style.backgroundColor = "#2e4053";
+      bordersName[i].style.color = "white";
+    }
+  } else {
+    document.getElementById("HomeBody").style.backgroundColor = "#ECEAEA";
+    let header = document.getElementById("HomeHeader");
+    document.getElementById("HomeHeader").style.backgroundColor = "white";
+    document.getElementById("HomeHeaderTitle").style.color = "#000000";
+    document.getElementById("HomeHeaderDarkMode").style.color = "#000000";
+    header.style.backgroundColor = "white";
+    document.getElementsByClassName("ViewCountryName")[0].style.color =
+      "#000000";
+    let keys = document.getElementsByClassName("ViewKey");
+    for (let i = 0; i < keys.length; i++) {
+      keys[i].style.color = "#000000";
+    }
+    document.getElementsByClassName("ViewCountryNativeName")[0].style.color =
+      "#000000";
+    document.getElementsByClassName("ViewCountryPopulation")[0].style.color =
+      "#000000";
+    document.getElementsByClassName("ViewCountryReigon")[0].style.color =
+      "#000000";
+    document.getElementsByClassName("ViewCountrySubReigon")[0].style.color =
+      "#000000";
+    document.getElementsByClassName("ViewCountryCapital")[0].style.color =
+      "#000000";
+    document.getElementsByClassName(
+      "ViewCountryTopLevelDomain"
+    )[0].style.color = "#000000";
+    document.getElementsByClassName("ViewCountryCurrencies")[0].style.color =
+      "#000000";
+    document.getElementsByClassName("ViewCountryLanguages")[0].style.color =
+      "#000000";
+    let nameCards = document.getElementsByClassName("nameCard");
+    let bordersName = document.getElementsByClassName("nameCardPara");
+    for (let i = 0; i < nameCards.length; i++) {
+      nameCards[i].style.backgroundColor = "white";
+      bordersName[i].style.color = "#000000";
+    }
+  }
+}
+
+function changeDarkMode() {
+  isDarkMode = !isDarkMode;
+  displayTheme(isDarkMode);
+}
